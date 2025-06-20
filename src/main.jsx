617d90ea8ef16332cb02539093b3da9e157f9aca
DineshBdk <dineshbdk0902@gmail.com>
2025-06-20 12:46:03 +0530

    Switched to BrowserRouter and fixed deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter} from "react-router-dom"
+import { BrowserRouter} from "react-router-dom"
 import { ScrollTop } from './components/ScrollTop.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename='/movie-explorer'>
+  <BrowserRouter basename='/movie-explorer'>
     <ScrollTop/>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
